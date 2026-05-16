bd5ef9f35234aa75f5e640b8b81992b00f5f297d
Safet Purkovic <86059488+dunpdev@users.noreply.github.com>
2026-05-16 19:34:40 +0200

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -90,10 +90,10 @@ DATABASES = {
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "obavezeDB",
-        "PASSWORD": "SoftInz1",
+        "NAME": "railway",
+        "PASSWORD": "vyCPoJMOmNDriMsCWitVoKoTwRnDcFsi",
         "USER": "postgres",
-        "HOST": "localhost",
+        "HOST": "postgres.railway.internal",
         "PORT": "5432",
     }
 }
