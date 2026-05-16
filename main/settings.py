877154bb4b6703e7becc6a5d97ea4147f695d663
Safet Purkovic <spurkovic@np.ac.rs>
2026-05-16 21:03:48 +0200

    Revert "Update settings.py"
    This reverts commit bd5ef9f35234aa75f5e640b8b81992b00f5f297d.

--- main/settings.py
+++ main/settings.py
@@ -90,10 +90,10 @@ DATABASES = {
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": BASE_DIR / "db.sqlite3",
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "PASSWORD": "vyCPoJMOmNDriMsCWitVoKoTwRnDcFsi",
+        "NAME": "obavezeDB",
+        "PASSWORD": "SoftInz1",
         "USER": "postgres",
-        "HOST": "postgres.railway.internal",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
